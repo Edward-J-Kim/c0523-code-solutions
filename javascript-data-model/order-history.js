c53954808f8c27fb053c84440b6983bfb389be96
Edward Kim <ewakim90@gmail.com>
2023-05-09 18:56:11 +0000

    Completed javascript-data-model exercise.

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,30 @@
+const orderHistory = [
+  {
+    orderNumber: '114-3941689-872232',
+    itemName: 'Javascript for impatient programmers',
+    totalCost: '31.55',
+    orderPlaced: '08042020',
+    expectedDeliveryDate: '08082020',
+  },
+  {
+    orderNumber: '113-9984268-1280257',
+    itemName: 'The Timeless Way of Building',
+    totalCost: '44.53',
+    orderPlaced: '07192020',
+    expectedDeliveryDate: '07202020',
+  },
+  {
+    orderNumber: '114-2875557-89059409',
+    itemName: 'Gamecube Controller Adapter',
+    totalCost: '17.22',
+    orderPlaced: '07042020',
+    expectedDeliveryDate: '07072020',
+  },
+  {
+    orderNumber: '113-2883177-2648248',
+    itemName: 'Gamecube Controller, etc',
+    totalCost: '138.93',
+    orderPlaced: '07032020',
+    expectedDeliveryDate: '07052020',
+  },
+];
