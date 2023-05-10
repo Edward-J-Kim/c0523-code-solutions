09597bd94301a53cc51ffaf5cb48151e755c26fe
Edward Kim <ewakim90@gmail.com>
2023-05-10 22:17:04 +0000

    Completed javascript-conditionals exercise.

---
+++ javascript-conditionals/javascript-conditionals-quiz-notes.md
@@ -0,0 +1,52 @@
+# javascript-conditionals-quiz-notes
+
+## Quiz Questions
+
+After completing this exercise, you should be able to discuss or answer the following questions:
+
+- Give 6 examples of comparison operators.
+  <, >, <=, >=, +=, ===
+- What data type do comparison expressions evaluate to?
+  s
+- What is the purpose of an `if` statement?
+  The if/else statement executes a block of code if a specified condition is true
+- Is `else` required in order to use an `if` statement?
+  it's not required
+- Describe the syntax (structure) of an `if` statement.
+  if statement with conditions and the return.
+- What are the three logical operators?
+  or, and , not
+- How do you compare two different expressions in the same condition?
+  by using the logical operators
+- What is the purpose of a `switch` statement?
+  The switch statement is used to test the equality of a variable against several values specified in the test cases
+- Is a `default` clause required in order to use a `switch` statement?
+  Not required.
+
+## Notes
+
+All student notes should be written here.
+
+How to write `Code Examples` in markdown
+
+for JS:
+
+```javascript
+const data = 'Howdy';
+```
+
+for HTML:
+
+```html
+<div>
+  <p>This is text content</p>
+</div>
+```
+
+for CSS:
+
+```css
+div {
+  width: 100%;
+}
+```
---
+++ javascript-conditionals/main.js
@@ -0,0 +1,94 @@
+/* exported isUnderFive,
+            isEven,
+            startsWithJ,
+            isOldEnoughToDrink,
+            isOldEnoughToDrive,
+            isOldEnoughToDrinkAndDrive,
+            categorizeAcidity,
+            introduceWarnerBro,
+            recommendMovie
+ */
+function isUnderFive(number) {
+  if (number < 5) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function isEven(number) {
+  if (number % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function startsWithJ(string) {
+  if (string[0] === 'J') {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function isOldEnoughToDrink(person) {
+  if (person.age >= 21) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function isOldEnoughToDrive(person) {
+  if (person.age >= 16) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function isOldEnoughToDrinkAndDrive(person) {
+  return false;
+}
+
+function categorizeAcidity(pH) {
+  if (pH === 7) {
+    return 'neutral';
+  } else if (pH > 0 && pH < 7) {
+    return 'acid';
+  } else if (pH > 7 && pH < 14) {
+    return 'base';
+  } else {
+    return 'invalid pH level';
+  }
+}
+
+function introduceWarnerBro(name) {
+  switch (name) {
+    case 'yakko':
+    case 'wakko':
+      return "We're the warner brothers!";
+    case 'dot':
+      return "I'm cute~";
+    default:
+      return 'Goodnight everybody!';
+  }
+}
+
+function recommendMovie(genre) {
+  switch (genre) {
+    case 'action':
+      return 'John Wick';
+    case 'comedy':
+      return 'Hangover';
+    case 'horror':
+      return 'Scream';
+    case 'drama':
+      return 'Forrest Gump';
+    case 'musical':
+      return 'La La Land';
+    case 'sci-fi':
+      return 'Minority Report';
+  }
+}
