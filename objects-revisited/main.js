37f4aa0fc65094db6915eaf3eb7ddc8c70e991f6
Edward Kim <ewakim90@gmail.com>
2023-06-19 20:50:41 +0000

    Completed objects-revisited.

---
+++ objects-revisited/main.js
@@ -0,0 +1,26 @@
+// const business= {
+//   opens: '8am',
+//   closes: '6pm',
+//   totalEmployees: 3,
+//   daysOpen: ['M','Tu','Th','F'],
+//   employees: {
+//     Ron : {
+//       position: 'cashier',
+//       daysOfWeekWorking: ['M','F'],
+//      },
+//      Ted: {
+//       position: 'cashier',
+//       daysOfWeekWorking: ['Tu','F'],
+//      },
+//      Ben: {
+//       position: 'cashier',
+//       daysOfWeekWorking: ['Tu','Th'],
+//      },
+//     }
+// };
+
+// function addWeekends(business){
+//   business.daysOpen = ['M','Tu','Th','F','Sat','Sun'];
+//   for(let i =0; i<business.employees.length;i++)
+//   business.employees.daysOfWeekWorking.push('Sat','Sun')
+// }
