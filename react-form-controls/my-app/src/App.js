2171b06ed3ee58ffe5fe7c3ab63409a4395162ab
Edward Kim <ewakim90@gmail.com>
2023-06-28 06:11:16 +0000

    Completed react-form-controls exercise.

---
+++ react-form-controls/my-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import RegistrationFormUncontrolled from './RegistrationFormUncontrolled';
+
+function App() {
+  return (
+    <div className="App">
+      <RegistrationFormUncontrolled />
+    </div>
+  );
+}
+
+export default App;
