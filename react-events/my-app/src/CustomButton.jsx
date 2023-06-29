e83537d18751a2c4cf6f87ac2574c39809550a56
Edward Kim <ewakim90@gmail.com>
2023-06-29 17:30:23 +0000

    Completed react-events exercise.

--- react-events/my-app/src/CustomButton.jsx
+++ react-events/my-app/src/CustomButton.jsx
@@ -1,3 +1,13 @@
-// export default CustomButton(){
+export default function CustomButton({ color, text }) {
+  function handleClick(text) {
+    window.alert(text);
+  }
 
-// }
+  return (
+    <button
+      style={{ backgroundColor: color }}
+      onClick={() => handleClick(text)}>
+      {text}
+    </button>
+  );
+}
